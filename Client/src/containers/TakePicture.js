e2bdbe9c5eca69da0bb4d3401dab34e526e7c55a
hsim <hsim@daou.co.kr>
2017-08-04 17:47:59 +0900

    사진 관련 기능 업데이트

--- Client/src/containers/TakePicture.js
+++ Client/src/containers/TakePicture.js
@@ -1,61 +1,156 @@
-'use strict';
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
 import {
-  AppRegistry,
-  Dimensions,
+  Image,
+  TouchableOpacity,
   StyleSheet,
-  Text,
-  TouchableHighlight,
   View,
-  Button
 } from 'react-native';
 import Camera from 'react-native-camera';
 import { bindActionCreators } from 'redux'
-import * as deviceActions from '../reducers/device/deviceActions'
+import * as photoActions from '../reducers/photo/photoActions'
+
+import {Actions} from 'react-native-router-flux'
+/**
+ *  Instead of including all app states via ...state
+ *  One could explicitly enumerate only those which Main.js will depend on.
+ *
+ */
 function mapStateToProps (state) {
   return {
-    deviceVersion: state.device.version
+    photo: {
+      photoList:state.photo.photoList
+    }
   }
 }
 
 /*
- * Bind all the actions in deviceActions
+ * Bind all the actions
  */
 function mapDispatchToProps (dispatch) {
   return {
-    actions: bindActionCreators(deviceActions, dispatch)
+    actions: bindActionCreators({ ...photoActions }, dispatch)
   }
 }
+
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'row',
   },
   preview: {
     flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
-  }
+  overlay: {
+    position: 'absolute',
+    padding: 16,
+    right: 0,
+    left: 0,
+    alignItems: 'center',
+  },
+  topOverlay: {
+    top: 0,
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  bottomOverlay: {
+    bottom: 0,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  captureButton: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderRadius: 40,
+  },
+  typeButton: {
+    padding: 5,
+  },
+  flashButton: {
+    padding: 5,
+  },
+  buttonsSpace: {
+    width: 10,
+  },
 });
-let TakePicture = React.createClass({
+
+export class TakePicture extends React.Component{
+
+
+   constructor(props) {
+    super(props);
+
+    this.state = {
+      camera: {
+        aspect: Camera.constants.Aspect.fill,
+        captureTarget: Camera.constants.CaptureTarget.temp,
+        captureQuality:'photo',
+        type: Camera.constants.Type.back,
+        orientation: Camera.constants.Orientation.auto,
+        flashMode: Camera.constants.FlashMode.auto,
+      },
+      isRecording: false
+    };
+
+
+    this.takePicture = this.takePicture.bind(this)
+    this.savePhoto = this.savePhoto.bind(this)
+  }
+ get typeIcon() {
+    let icon;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      icon = require('../../assets/ic_camera_rear_white.png');
+    } else if (this.state.camera.type === front) {
+      icon = require('../../assets/ic_camera_front_white.png');
+    }
+
+    return icon;
+  }
+  
+  switchType = () => {
+    let newType;
+    const { back, front } = Camera.constants.Type;
+
+    if (this.state.camera.type === back) {
+      newType = front;
+    } else if (this.state.camera.type === front) {
+      newType = back;
+    }
+
+    this.setState({
+      camera: {
+        ...this.state.camera,
+        type: newType,
+      },
+    });
+  }
+
+  savePhoto(data) {
+    this.props.actions.setPhoto(data);
+  }
   takePicture() {
-    console.log('click')
-    console.log(this.camera)
+ 
+    console.log("camera : " + this.camera)
     if (this.camera) {
       this.camera.capture()
-        .then((data) => console.log(data))
+        .then((data) => {
+          console.log(data)
+          console.log(data.path)
+          this.savePhoto(data.path)
+        }
+        )
         .catch(err => console.error(err));
     }
-  },
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -64,12 +159,31 @@ let TakePicture = React.createClass({
             this.camera = cam;
           }}
           style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}>
-          <Button onPress={this.takePicture} title="CAPTURE">[CAPTURE]</Button>
-        </Camera>
+          aspect={this.state.camera.aspect}
+          captureTarget={this.state.camera.captureTarget}
+          type={this.state.camera.type}
+          flashMode={this.state.camera.flashMode}
+          captureQuality={this.state.camera.captureQuality}
+          onFocusChanged={() => {}}
+          onZoomChanged={() => {}}
+          defaultTouchToFocus
+          mirrorImage={false}
+
+        />
+
+          <View style={[styles.overlay, styles.bottomOverlay]}>
+            <TouchableOpacity
+                style={styles.captureButton}
+                onPress={this.takePicture}
+            >
+              <Image
+                  source={require('../../assets/ic_photo_camera_36pt.png')}
+              />
+            </TouchableOpacity>
+          </View>
       </View>
     );
   }
-})
+}
 
 export default connect(mapStateToProps, mapDispatchToProps)(TakePicture)
--- Client/src/reducers/index.js
+++ Client/src/reducers/index.js
@@ -13,6 +13,7 @@
 import auth from './auth/authReducer'
 import device from './device/deviceReducer'
 import global from './global/globalReducer'
+import photo from './photo/photoReducer'
 
 import { combineReducers } from 'redux'
 
@@ -26,6 +27,7 @@ const rootReducer = combineReducers({
   auth,
   device,
   global,
+  photo,
 })
 
 export default rootReducer
