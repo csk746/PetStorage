20bda88760fb5187e1ad1fd971bf91090f2ac89b
gloryan0829 <candoublej@gmail.com>
2017-08-11 11:18:35 +0900

    no message

--- Client/src/containers/Login.js
+++ Client/src/containers/Login.js
@@ -9,8 +9,8 @@
  *
  * Imports from redux
  */
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 
 /**
  * The actions we need
@@ -33,12 +33,12 @@ import {Actions} from 'react-native-router-flux'
 import React, {Component} from 'react'
 import
 {
-  StyleSheet,
-  View,
-  Text,
-  TextInput
+    StyleSheet,
+    View,
+    Text,
+    TextInput
 }
-from 'react-native'
+    from 'react-native'
 
 /**
  * The platform neutral button
@@ -50,61 +50,62 @@ const Button = require('apsl-react-native-button')
  *  One could explicitly enumerate only those which Main.js will depend on.
  *
  */
-function mapStateToProps (state) {
-  return {
-    auth: {
-      form: {
-        isFetching: state.auth.form.isFetching
-      }
-    },
-    global: {
-      currentState: state.global.currentState,
-      showState: state.global.showState
+function mapStateToProps(state) {
+    return {
+        auth: {
+            form: {
+                isFetching: state.auth.form.isFetching
+            }
+        },
+        global: {
+            currentState: state.global.currentState,
+            showState: state.global.showState
+        }
     }
-  }
 }
 
 /*
  * Bind all the actions
  */
-function mapDispatchToProps (dispatch) {
-  return {
-    actions: bindActionCreators({ ...authActions, ...globalActions }, dispatch)
-  }
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators({...authActions, ...globalActions}, dispatch)
+    }
 }
 
 var styles = StyleSheet.create({
-  container: {
-    flexDirection: 'column',
-    flex: 1
-  },
-  summary: {
-    fontFamily: 'BodoniSvtyTwoITCTT-Book',
-    fontSize: 18,
-    fontWeight: 'bold'
-  },
-  button: {
-    backgroundColor: '#FF3366',
-    borderColor: '#FF3366',
-    marginLeft: 10,
-    marginRight: 10
-    // marginTop:520
-  },
-  picture: {
-    backgroundColor: '#FF3366',
-    borderColor: '#FF3366',
-    marginLeft: 40,
-    marginRight: 40,
-    // position:'absolute',
-    marginTop:520
-    // marginBottom:10
-  }
+    container: {
+        flexDirection: 'column',
+        flex: 1
+    },
+    summary: {
+        fontFamily: 'BodoniSvtyTwoITCTT-Book',
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    button: {
+        backgroundColor: '#FF3366',
+        borderColor: '#FF3366',
+        marginLeft: 10,
+        marginRight: 10
+        // marginTop:520
+    },
+    picture: {
+        backgroundColor: '#FF3366',
+        borderColor: '#FF3366',
+        marginLeft: 40,
+        marginRight: 40,
+        // position:'absolute',
+        marginTop: 520
+        // marginBottom:10
+    }
 })
 /**
  * ### Translations
  */
 var I18n = require('react-native-i18n')
 import Translations from '../lib/Translations'
+
 I18n.translations = Translations
 
 /**
@@ -112,44 +113,46 @@ I18n.translations = Translations
  */
 class Login extends Component {
 
-handlePress () {
-    Actions.Subview({
-      title: 'Subview'
-      // you can add additional props to be passed to Subview here...
-    })
-  }
-takePicture () {
-    Actions.TakePicture({
-      title: 'TakePicture'
-      // you can add additional props to be passed to Subview here...
-    })
-  }
-goToMain () {
-    Actions.Main({
-      // title: 'Main'
-      // you can add additional props to be passed to Subview here...
-    })
-  }
+    handlePress() {
+        Actions.Subview({
+            title: 'Subview'
+            // you can add additional props to be passed to Subview here...
+        })
+    }
 
-  render () {
-    return (
-      <View style={styles.container}>
-        <Text>로그인화면</Text> 
-        {/* <Button></Button> */}
-        <Text>ID</Text><TextInput></TextInput>
-        <Text>PW</Text><TextInput></TextInput>
-        <Button onPress = {this.goToMain}>로그인</Button>
+    takePicture() {
+        Actions.TakePicture({
+            title: 'TakePicture'
+            // you can add additional props to be passed to Subview here...
+        })
+    }
 
-        <View>
-        
-          {/* <Button style={styles.button} onPress={this.handlePress.bind(this)}>
+    goToMain() {
+        Actions.Main({
+            // title: 'Main'
+            // you can add additional props to be passed to Subview here...
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>로그인화면</Text>
+                {/* <Button></Button> */}
+                <Text>ID</Text><TextInput></TextInput>
+                <Text>PW</Text><TextInput></TextInput>
+                <Button onPress={this.goToMain}>로그인</Button>
+
+                <View>
+
+                    {/* <Button style={styles.button} onPress={this.handlePress.bind(this)}>
             {I18n.t('Main.navigate')}
           </Button> */}
 
-        </View>
-      </View>
-    )
-  }
+                </View>
+            </View>
+        )
+    }
 }
 
 /**
