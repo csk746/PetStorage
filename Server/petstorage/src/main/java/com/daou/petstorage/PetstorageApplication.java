53033e658ec7badc2269a9167c3e46c7440c93c3
geonhee lee <csk746@gmail.com;git config --global user.email csk746@gmail.com>
2017-08-11 13:36:30 +0900

    서버 초기세팅

---
+++ Server/petstorage/src/main/java/com/daou/petstorage/PetstorageApplication.java
@@ -0,0 +1,12 @@
+package com.daou.petstorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetstorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetstorageApplication.class, args);
+	}
+}
